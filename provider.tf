5f865fec37f6c0cf315b708cb9ce315268b334de
MuchTest <florentina_jd@hotmail.com>
2021-03-22 13:34:33 +0000

    Example 3 - fix

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid = " " # here you add your tenancy ocid
+  user_ocid = " " # here you add your user ocid
+  private_key_path = " " # here you add your .oci key path
+  fingerprint = " " # here you add your fingerprint
+  region = "eu-frankfurt-1" # here you add your region
+}
